43892c36cd22fbc212fc28a3e949dedd243c2700
Vo Anh Dung <vodung0801@gmail.com>
2020-04-15 16:10:00 +0700

    Example TodoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'Có làm thì mới có ăn!' },
+    { id: 2, title: 'Không làm mà đòi ăn!' },
+    { id: 3, title: 'Chỉ có ăn ...!' },
+  ]);
+
+  const handleTodoClick = (todo) => {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
       <h1>Welcome to React Hooks!</h1>
-      <ColorBox />
+      {/* <ColorBox /> */}
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
